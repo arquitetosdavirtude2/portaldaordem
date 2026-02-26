d42a725405b93c1a6a38acf5c413e7c3424c458c
Michel <ti@arquitetosdavirtude2.com.br>
2026-02-26 18:03:20 -0300

    feat: Add Next.js configuration for static export, disabled SWC minification, and unoptimized images.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,9 +2,11 @@
 const nextConfig = {
     swcMinify: false, // Force disable SWC minification
     eslint: {
-        // Warning: This allows production builds to successfully complete even if
-        // your project has ESLint errors.
         ignoreDuringBuilds: true,
+    },
+    output: 'export',
+    images: {
+        unoptimized: true,
     },
 };
 
